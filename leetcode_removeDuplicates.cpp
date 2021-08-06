0186d0ef7a392facb145924dcac479ebed6de8d0
shenmingyi <https://gitlab.dev.ishunwan.com/>
2021-08-06 17:48:53 +0800

    removeDuplicates

---
+++ leetcode_removeDuplicates.cpp
@@ -0,0 +1,30 @@
+﻿// leetcode.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+#include "math.h"
+#include <vector>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <functional>
+
+
+int removeDuplicates(std::vector<int>& nums) {
+
+    std::sort(nums.begin(), nums.end());
+
+    auto it = std::unique(nums.begin(), nums.end());
+
+    nums.erase(it, nums.end());
+
+    return nums.size();
+}
+
+
+int main()
+{
+    system("pause");
+}
+
