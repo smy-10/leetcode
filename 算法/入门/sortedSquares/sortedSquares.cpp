1221513dbcf81d1a11b8c025fead29832435dc45
10 <33865665+smy-10@users.noreply.github.com>
2021-10-28 15:06:10 +0800

    .

---
+++ 算法/入门/sortedSquares/sortedSquares.cpp
@@ -0,0 +1,75 @@
+﻿// sortedSquares.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*
+给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+
+ 
+
+示例 1：
+
+输入：nums = [-4,-1,0,3,10]
+输出：[0,1,9,16,100]
+解释：平方后，数组变为 [16,1,0,9,100]
+排序后，数组变为 [0,1,9,16,100]
+示例 2：
+
+输入：nums = [-7,-3,2,3,11]
+输出：[4,9,9,49,121]
+ 
+
+提示：
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums 已按 非递减顺序 排序
+ 
+
+进阶：
+
+请你设计时间复杂度为 O(n) 的算法解决本问题
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/squares-of-a-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> sortedSquares(vector<int>& nums) 
+	{
+		vector<int> result;
+
+		for (const auto& num : nums)
+		{
+			result.push_back(num * num);
+		}
+
+		sort(result.begin(), result.end());
+
+		return result;
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> vec{ -4,-1,0,3,10 };
+	auto a = s.sortedSquares(vec);
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
